8a6433f76c2c62bf2d75579aaef56edda35e5ac8
Nikki <piknik@me.com>
2020-10-23 13:04:17 -0600

    did some formatting

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Card({ card }) {
+    const [flip, setFlip] = useState(false);
     return (
-        <div>
-            {card.question}
-
+        <div
+            className={`card ${flip ? 'flip' : ''}`}
+            onClick={() => setFlip(!flip)}
+        >
+            <div className="front">
+                {card.question}
+                {card.options.map(option => <div className="card-option">{option}</div>)}
+            </div>
+            <div className="back">
+                {card.answer}
+            </div>
         </div>
     )
 }
